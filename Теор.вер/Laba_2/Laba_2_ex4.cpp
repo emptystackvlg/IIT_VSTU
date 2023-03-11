4dd0a27d28d12f2a9ce9f0a36e4ff34ebf191650
Krykov Alexandr <57997351+emptystackvlg@users.noreply.github.com>
2023-03-11 16:01:45 +0300

    Сделал 4-ый номер  2-ой лабы

---
+++ Теор.вер/Laba_2/Laba_2_ex4.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+
+using namespace std;
+
+float main_prob()
+{
+	int num_of_events;
+	cout << "Введите число событий, образующих полную группу\n> ";
+	cin >> num_of_events;
+	float probs_of_hyps[100];
+	float cond_probs[100];
+	float sum_of_probs_of_hyps = 0;
+	
+	while (sum_of_probs_of_hyps !=1)
+	{
+		sum_of_probs_of_hyps = 0;
+		system("cls");
+		for (int i = 0; i < num_of_events; i++)
+		{
+			cout << "Введите вероятность гипотезы H_" << i + 1 << "\n> ";
+			cin >> probs_of_hyps[i];
+			sum_of_probs_of_hyps += probs_of_hyps[i];
+		}
+		if (sum_of_probs_of_hyps > 1 || sum_of_probs_of_hyps < 0)
+		{
+			cout << "Введены неверные значения вероятностей" << endl;
+			system("pause");
+		}
+	}
+	cout << endl;
+	system("pause");
+	system("cls");
+	for (int i = 0; i < num_of_events; i++)
+	{
+		cout << "Введите условную вероятность события A относительно гипотезы H_" << i + 1 <<"\n> ";
+		cin >> cond_probs[i];
+	}
+	cout << endl;
+	system("pause");
+	int mode = 0;
+	while (mode != 1 && mode != 2)
+	{
+		float full_prob = 0;
+		for (int i = 0; i < num_of_events; i++)
+		{
+			full_prob += probs_of_hyps[i] * cond_probs[i];
+		}
+		system("cls");
+		cout << "Выберите формулу для расчета вероятности:\n 1 - формула полной вероятности\n 2 - формула Байеса\n Введите 0 для выхода\n> ";
+		cin >> mode;
+		if (mode == 0) return 0;
+		else if (mode == 1)
+		{
+			
+			system("cls");
+			cout << "Выбрана формула полной вероятности\n\n";
+			cout << "P(A) = сумма по i от 0 до n (P(Hi) * P(Hi|A)) " << "\n\n";
+			cout << "Вероятность равна:  " << full_prob << endl;
+			system("pause");
+		}
+		else if (mode == 2)
+		{
+			system ("cls");
+			cout << "Выбрана формула Байеса\n\n";
+			cout << "P(Hi|A) = (P(Hi)*P(A|Hi))/P(A)\n\n";
+			float hyp_prob = 0;
+			for (int i = 0; i < num_of_events; i++)
+			{
+				hyp_prob = 0;
+				hyp_prob = (probs_of_hyps[i] * cond_probs[i]) / full_prob;
+				cout << "Вероятность гипотезы H_" << i + 1 << " при наступлении события A : " << hyp_prob << "\n\n";
+				system("pause");
+			}
+		}
+		
+	}
+	return 0;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	main_prob();
+	
+}
