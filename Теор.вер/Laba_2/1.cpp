0dbf4718329408c9e425d2d1302db782f8f659f6
Krykov Alexandr <57997351+emptystackvlg@users.noreply.github.com>
2023-03-21 17:39:09 +0300

    Загрузил  задания 1 и 2 для второй лабы

---
+++ Теор.вер/Laba_2/1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std ;
+void prob ()
+{
+    float probs [100];
+    for (int i = 0 ; i <6;i++)
+    {
+        cout << "Введите вероятность отказа элемента № " << i+1<<"\n> ";
+        cin >> probs[i];
+        cout << "\n";
+    }
+    
+    //system ("clear");
+    cout << "Формула для события B-схема работает безотказно в течение времени T :"<<"\n\n";
+    cout << "B = A1 * (A2 + (A4 * A5 * A6)) * A3" << "\n\n";
+    cout <<"Формула для расчета вероятности события B" <<"\n\n";
+    cout << "P(B) = (1 -P(A1)) * ((1-P(A2)) + ((1-P(A4)) * (1-P(A5)) * (1-P(A6)))) * (1-P(A3))"<<"\n\n";
+    float prob_of_b;
+    prob_of_b = (1 - probs[0]) * ((1 - probs[1]) + ((1-probs[3]) * (1-probs[4]) * (1-probs[5]))) * (1-probs [2]);
+    cout << "Вероятность события B =" << prob_of_b;
+    
+    
+}
+
+
+
+int main()
+{
+    setlocale (LC_ALL,"rus");
+    prob ();
+  
+}
