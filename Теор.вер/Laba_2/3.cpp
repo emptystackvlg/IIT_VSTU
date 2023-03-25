0421a1c16522a570e2c2fda91515506397bf4f4a
Krykov Alexandr <57997351+emptystackvlg@users.noreply.github.com>
2023-03-25 10:30:16 +0300

    Update 3.cpp

--- Теор.вер/Laba_2/3.cpp
+++ Теор.вер/Laba_2/3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void prob()
 {
     cout << " Пять стрелков производят по одному выстрелу в цель. Вероятности попадания в цель i-ым стрелком соответственно равны pi.\n Найти вероятность попадания в цель:\n a) только i-го стрелка\n b) только одного стрелка\n c) только двух стрелков\n d) не менее четырех стрелков\n";
-    cout << "\n\nФормулы для вычисления вероятностей\n a) P(a) = ~pi*q1*q2*q3*q4, где ~pi - вероятность попадания i-го стрелка\n\n b,c,d,e) P (b,c,d,e) = (p1*x + q1) *  (p2*x + q2) *  (p3*x + q3) *  (p4*x + q4) *  (p5*x + q5)\n где \n P(b) = коэфф при x\n P(c) = коэфф при x^2\n P(d) = коэфф при x^4 + коэфф при x^5  \n P(e) = коэфф при x + коэфф при x^2 + коэфф при x^3 +коэфф при x^4 + коэфф при x^5" << "\n\n";
+    cout << "\n\nФормулы для вычисления вероятностей\n a) P(a) = ~pi*q1*q2*q3*q4, где ~pi - вероятность попадания i-го стрелка\n\n b,c,d,e) P (b,c,d,e) = (p1*x + q1) *  (p2*x + q2) *  (p3*x + q3) *  (p4*x + q4) *  (p5*x + q5)\n где \n P(b) = коэфф при x\n P(c) = коэфф при x^2\n P(d) = коэфф при x^4 + коэфф при x^5  \n P(e) = 1 - коэфф при x^0" << "\n\n";
     float prob_of_hit[5];
     float prob_of_miss[5];
     for (int i = 0; i < 5; i++)
@@ -14,7 +14,7 @@ void prob()
         prob_of_miss[i] = 1 - prob_of_hit[i];
     }
     cout << "Введите номер стрелка для пункта a\n> ";
-    int num = 0; 
+    int num = 0;
     cin >> num;
     float four_miss = 1;
     for (int i = 0; i < 5; i++)
@@ -26,14 +26,14 @@ void prob()
     float odds[6];
     float five = prob_of_hit[0] * prob_of_hit[1] * prob_of_hit[2] * prob_of_hit[3] * prob_of_hit[4];
 
-    float four = 
+    float four =
         prob_of_hit[0] * prob_of_hit[1] * prob_of_hit[2] * prob_of_hit[3] * prob_of_miss[4] +
         prob_of_hit[0] * prob_of_hit[1] * prob_of_hit[2] * prob_of_hit[4] * prob_of_miss[3] +
         prob_of_hit[0] * prob_of_hit[1] * prob_of_hit[3] * prob_of_hit[4] * prob_of_miss[2] +
         prob_of_hit[0] * prob_of_hit[2] * prob_of_hit[3] * prob_of_hit[4] * prob_of_miss[1] +
         prob_of_hit[1] * prob_of_hit[2] * prob_of_hit[3] * prob_of_hit[4] * prob_of_miss[0];
 
-    float three = 
+    float three =
         prob_of_hit[0] * prob_of_hit[1] * prob_of_hit[2] * prob_of_miss[3] * prob_of_miss[4] +
         prob_of_hit[0] * prob_of_hit[1] * prob_of_hit[3] * prob_of_miss[2] * prob_of_miss[4] +
         prob_of_hit[0] * prob_of_hit[1] * prob_of_hit[4] * prob_of_miss[2] * prob_of_miss[3] +
@@ -41,9 +41,9 @@ void prob()
         prob_of_hit[0] * prob_of_hit[2] * prob_of_hit[4] * prob_of_miss[1] * prob_of_miss[3] +
         prob_of_hit[0] * prob_of_hit[3] * prob_of_hit[4] * prob_of_miss[1] * prob_of_miss[2] +
         prob_of_hit[1] * prob_of_hit[2] * prob_of_hit[3] * prob_of_miss[0] * prob_of_miss[4] +
-        prob_of_hit[1] * prob_of_hit[2] * prob_of_hit[4] * prob_of_miss[0] * prob_of_miss[3] + 
-        prob_of_hit[1] * prob_of_hit[3] * prob_of_hit[4] * prob_of_miss[0] * prob_of_miss[2] + 
-        prob_of_hit[2] * prob_of_hit[3] * prob_of_hit[4] * prob_of_miss[0] * prob_of_miss[1] ;
+        prob_of_hit[1] * prob_of_hit[2] * prob_of_hit[4] * prob_of_miss[0] * prob_of_miss[3] +
+        prob_of_hit[1] * prob_of_hit[3] * prob_of_hit[4] * prob_of_miss[0] * prob_of_miss[2] +
+        prob_of_hit[2] * prob_of_hit[3] * prob_of_hit[4] * prob_of_miss[0] * prob_of_miss[1];
 
     float two =
         prob_of_hit[0] * prob_of_hit[1] * prob_of_miss[2] * prob_of_miss[3] * prob_of_miss[4] +
@@ -71,12 +71,13 @@ void prob()
     odds[3] = three;
     odds[4] = four;
     odds[5] = five;
+    cout << "Вероятности равны : \n";
     cout << "a) " << i_hit << "\n\n";
     cout << "b) " << odds[1] << "\n\n";
     cout << "c) " << odds[2] << "\n\n";
     cout << "d) " << odds[4] + odds[5] << "\n\n";
-    cout << "d) " << 1 - odds [0] << "\n\n";
-   
+    cout << "d) " << 1 - odds[0] << "\n\n";
+
 }
 
 int main()
