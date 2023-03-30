893f2830239a05d93add241f50c17748f26d3afc
Krykov Alexandr <57997351+emptystackvlg@users.noreply.github.com>
2023-03-30 19:34:55 +0300

    Исправление формул

--- Теор.вер/Laba_2/1.cpp
+++ Теор.вер/Laba_2/1.cpp
@@ -3,27 +3,29 @@
 using namespace std;
 void prob()
 {
-    float probs[100];
-    cout << "Схема :" << "\n\n";
-    cout << "                   ---------------|   2   |---------------" << endl;
-    cout << "                 |                                        |" << endl;
-    cout << "o---|   1   |----|                                        |----|   3   |---o" << endl;
-    cout << "                 |                                        |" << endl;
-    cout << "                   ---|   4   |---|   5   |---|   6   |---" << endl;
-    cout << "\n\nФормула для события B-схема работает безотказно в течение времени T :" << "\n\n";
-    cout << "B = !A1 * (!A2 + (!A4 * !A5 * !A6)) * !A3" << "\n\n";
-    cout << "Формула для расчета вероятности события B" << "\n\n";
-    cout << "P(B) = (1 -P(A1)) * ((1-P(A2)) + ((1-P(A4)) * (1-P(A5)) * (1-P(A6)))) * (1-P(A3))" << "\n\n";
-    for (int i = 0; i < 6; i++)
-    {
-        cout << "Введите вероятность отказа элемента № " << i + 1 << "\n> ";
-        cin >> probs[i];
-        cout << "\n";
-    }
-    float prob_of_b;
-    prob_of_b = (1 - probs[0]) * ((1 - probs[1]) + ((1 - probs[3]) * (1 - probs[4]) * (1 - probs[5]))) * (1 - probs[2]);
-    cout << "Вероятность события B = " << prob_of_b<<"\n\n";
-    system("pause");
+	float probs[100];
+	cout << "Схема :" << "\n\n";
+	cout << "                   ---------------|   2   |---------------" << endl;
+	cout << "                 |                                        |" << endl;
+	cout << "o---|   1   |----|                                        |----|   3   |---o" << endl;
+	cout << "                 |                                        |" << endl;
+	cout << "                   ---|   4   |---|   5   |---|   6   |---" << endl;
+	cout << "\n\nФормула для события B-схема работает безотказно в течение времени T :" << "\n\n";
+	cout << "B = !A1 * ((!A2 + (!A4 * !A5 * !A6)) - !A2 * A4 * A5 * A6) * !A3" << "\n\n";
+	cout << "Формула для расчета вероятности события B" << "\n\n";
+	cout << "P(B) = (1 -P(A1)) * ((1-P(A2)) + ((1-P(A4)) * (1-P(A5)) * (1-P(A6))) - ((1-P(A2)) * ((1-P(A4)) * (1-P(A5)) * (1-P(A6)) * (1-P(A3))" << "\n\n";
+	for (int i = 0; i < 6; i++)
+	{
+		cout << "Введите вероятность отказа элемента № " << i + 1 << "\n> ";
+		cin >> probs[i];
+		cout << "\n";
+	}
+	float prob_of_b;
+	float minus = (1 - probs[1]) * (1 - probs[3]) * (1 - probs[4]) * (1 - probs[5]);
+	prob_of_b = (1 - probs[0]) * ((1 - probs[1]) + ((1 - probs[3]) * (1 - probs[4]) * (1 - probs[5])) - minus) * (1 - probs[2]);
+	cout << "Вероятность события B = " << prob_of_b << "\n\n";
+	system("pause");
+
 
 
 }
@@ -32,7 +34,7 @@ void prob()
 
 int main()
 {
-    setlocale(LC_ALL, "rus");
-    prob();
+	setlocale(LC_ALL, "rus");
+	prob();
 
 }
--- Теор.вер/Laba_2/2.cpp
+++ Теор.вер/Laba_2/2.cpp
@@ -3,31 +3,41 @@
 using namespace std;
 void prob()
 {
-    float probs[100];
-    cout << "Схема:" << "\n\n";
-    cout << "       -|   2   |- " << endl;
-    cout << "      |           |" << endl;
-    cout << "     -             -|   3   |---|   4   |---" << endl;
-    cout << "    | |           |                         |" << endl;
-    cout << "о---|  -|   1   |-                           ---o" << endl;
-    cout << "    |                                       |" << endl;
-    cout << "     ---|   5   |---|   6   |---------------" << endl;
-    cout << "\n\nФормула для события B - схема работает безотказно в течение времени T :" << "\n\n";
-    cout << "B = ((!A1 + !A2) * !A3 * !A4) + (!A5 * !A6) " << "\n\n";
-    cout << "Формула для расчета вероятности события B" << "\n\n";
-    cout << "P(B) = ((1 - P(A1)) + (1 - P(A2)) * (1 - P(A3)) * (1 - P(A4))) + ((1- P(A5)) * (1 - P(A6)))" << "\n\n";
-    
-    for (int i = 0; i < 6; i++)
-    {
-        cout << "Введите вероятность отказа элемента № " << i + 1 << "\n> ";
-        cin >> probs[i];
-        cout << "\n";
-    }
+	float probs[100];
+	cout << "Схема:" << "\n\n";
+	cout << "       -|   2   |- " << endl;
+	cout << "      |           |" << endl;
+	cout << "     -             -|   3   |---|   4   |---" << endl;
+	cout << "    | |           |                         |" << endl;
+	cout << "о---|  -|   1   |-                           ---o" << endl;
+	cout << "    |                                       |" << endl;
+	cout << "     ---|   5   |---|   6   |---------------" << endl;
+	cout << "\n\nФормула для события B - схема работает безотказно в течение времени T :" << "\n\n";
+	cout << "B = ((!A1 + !A2 - !A1 * !A2) * !A3 * !A4) + (!A5 * !A6) " << "\n\n";
+	cout << "Формула для расчета вероятности события B" << "\n\n";
+	cout << "P(B) = ((1 - P(A1)) + (1 - P(A2) - (1 - P(A1)) * (1 - P(A2))  * (1 - P(A3)) * (1 - P(A4))) + ((1- P(A5)) * (1 - P(A6)))" << "\n\n";
 
-    float prob_of_b;
-    prob_of_b = (((1 - probs[0]) + (1 - probs[1])) * (1 - probs[2]) * (1 - probs[3])) + ((1 - probs[4]) * (1 - probs[5]));
-    cout << "Вероятность события B = " << prob_of_b << "\n\n";
-    system("pause");
+	for (int i = 0; i < 6; i++)
+	{
+		cout << "Введите вероятность отказа элемента № " << i + 1 << "\n> ";
+		cin >> probs[i];
+		cout << "\n";
+	}
+	float non_probs[100];
+	for (int i = 0; i < 6; i++)
+	{
+		non_probs[i] = 1 - probs[i];
+		cout << non_probs [i] << "\n\n"; 
+
+	}
+
+	float prob_of_b;
+	float one = (non_probs[0] + non_probs[1]) - non_probs[0] * non_probs[1];
+	float two = non_probs [2] * non_probs[3];
+	float three = non_probs[4] * non_probs[5];
+	prob_of_b = one * two + three;
+	cout << "Вероятность события B = " << prob_of_b << "\n\n";
+	system("pause");
 
 }
 
@@ -35,7 +45,7 @@ void prob()
 
 int main()
 {
-    setlocale(LC_ALL, "rus");
-    prob();
+	setlocale(LC_ALL, "rus");
+	prob();
 
 }
